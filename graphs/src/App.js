94e7b7253268177213183c5e3bf7c8e46fd11a8c
Danillo Souza <danillosouza1704@gmail.com>
2021-02-15 16:47:17 -0300

    Added board
    Co-authored-by: Jhonson <isudjoao@gmail.com>

--- graphs/src/App.js
+++ graphs/src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  
+  const renderTable = () => {
+    
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>FFF (Flood Fuck*ng Fill)</h1>
+      <section className="board">
+        <div className="cross">
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+        </div>
+        <div className="cross">
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+        </div>
+        <div className="cross">
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+        </div>
+        <div className="cross">
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+        </div>
+        <div className="cross">
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+        </div>
+        <div className="cross">
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+        </div>
+        <div className="cross">
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+        </div>
+        <div className="cross">
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+          <div className="field"></div>
+        </div>
+      </section >
+    </>
   );
-}
+};
 
 export default App;
